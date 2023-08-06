a1acbdae608d227b1820fbcd561ca95497eaff9c
TanapatButsai <tanapat.bu@ku.th>
2023-08-06 21:16:59 +0700

    board

--- src/MGame.java
+++ src/MGame.java
@@ -2,9 +2,10 @@ import java.util.ArrayList;
 
 public class MGame {
     private int roundCat;
-
+    private Board board = new Board();
     public void playGame(ArrayList<Player> players){
         System.out.println("Play game!!!!");
+        board.getSquare();
     }
 
     public void playRound(){
