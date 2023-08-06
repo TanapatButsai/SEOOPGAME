fdabbda31a4b8fd9d199a1713a266209153f6ec8
TanapatButsai <tanapat.bu@ku.th>
2023-08-06 20:55:09 +0700

    test

--- src/MGame.java
+++ src/MGame.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+
 public class MGame {
     private int roundCat;
 
-    public void playGame(){
-
+    public void playGame(ArrayList<Player> players){
+        System.out.println("Play game!!!!");
     }
 
     public void playRound(){
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Board board = new Board();
+        MGame mGame = new MGame();
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(player1);
+        players.add(player2);
+        players.add(player3);
+        mGame.playGame(players);
     }
 }
